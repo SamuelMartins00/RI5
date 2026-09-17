1f266cd3743c3e79a1cd87d0a9667d61d140464a
SamuelMartins00 <samuelfatec21@gmail.com>
2026-09-17 12:20:24 -0300

    feat
    some class in Javascript

---
+++ empresa.js
@@ -0,0 +1,9 @@
+export class Empresa{
+    constructor (razaoSocial, nomeFantasia, cnpj, endereco, funcionario){
+        this.razaoSocial = razaoSocial
+        this.nomeFantasia = nomeFantasia
+        this.cnpj = cnpj
+        this.endereco = endereco
+        this.funcionario = funcionario
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import Endereco from "./endereco";
+import Telefone from "./telefone";
+
+let endereco = new Endereco(123,'Av Paulista', 'Jardim Paulista', 'São Paulo')
+let telefone = new Telefone('011', '9-9999-9999')
+let funcionario = new Funcionario('Tony Stark', '123456789', '999.999.999.')
